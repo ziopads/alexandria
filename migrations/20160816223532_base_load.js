55c2b51d5e41befc6399ba678d720c168b531395
J. Haskins <ziopads@gmail.com>
2016-08-17 22:59:37 -0600

    update migrations file

--- migrations/20160816223532_base_load.js
+++ migrations/20160816223532_base_load.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex, Promise) {
-  
+  return PromiseAll([
+    // CREATE USERS TABLE
+    knex.schema.createTable('users', (table) => {
+      table.increments();
+      table.string('username').unique().notNullable();
+      table.string('email');
+      table.string('password');
+      table.boolean('isGod').defaultTo(false);
+      table.string('firstname');
+      table.string('lastname');
+      table.string('phoneNumber');
+    })
+    // CREATE BOOKS TABLE
+
+    // CREATE AUTHORS TABLE
+    // CREATE GENRES TABLE
+    // CREATE BOOKS_AUTHORS TABLE
+    // CREATE BOOKS_GENRES TABLE
+
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
